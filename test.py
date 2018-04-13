03d6ff28bfd8751437a86ec6c307c4eaa714eefe
Rafael Ribeiro <rafaelektron@gmail.com>
2018-04-13 02:07:16 -0300

    Update test.py

--- test.py
+++ test.py
@@ -4,26 +4,26 @@ import unittest
 
 class TestPolo(unittest.TestCase):
 
-    def test_method_integrity(self):
-        self.polo = poloniex.Poloniex()
-        for command in poloniex.PUBLIC_COMMANDS:
-            self.assertTrue(hasattr(self.polo, command))
-        for command in poloniex.PRIVATE_COMMANDS:
-            self.assertTrue(hasattr(self.polo, command))
-        self.assertTrue(hasattr(self.polo, 'marketTradeHist'))
+    #def test_method_integrity(self):
+    #    self.polo = poloniex.Poloniex()
+    #    for command in poloniex.PUBLIC_COMMANDS:
+    #        self.assertTrue(hasattr(self.polo, command))
+    #    for command in poloniex.PRIVATE_COMMANDS:
+    #        self.assertTrue(hasattr(self.polo, command))
+    #    self.assertTrue(hasattr(self.polo, 'marketTradeHist'))
 
-    def test_coach_existance(self):
-        self.polo = poloniex.Poloniex()
-        # coach is created by default
-        self.assertTrue(isinstance(self.polo.coach, poloniex.Coach))
-        # remove coach
-        self.polo = poloniex.Poloniex(coach=False)
-        self.assertFalse(self.polo.coach)
-        # coach injection
-        myCoach = poloniex.Coach()
-        self.polo = poloniex.Poloniex(coach=myCoach)
-        self.polo2 = poloniex.Poloniex(coach=myCoach)
-        self.assertTrue(self.polo.coach is self.polo2.coach)
+    #def test_coach_existance(self):
+    #    self.polo = poloniex.Poloniex()
+    #    # coach is created by default
+    #    self.assertTrue(isinstance(self.polo.coach, poloniex.Coach))
+    #    # remove coach
+    #    self.polo = poloniex.Poloniex(coach=False)
+    #    self.assertFalse(self.polo.coach)
+    #    # coach injection
+    #    myCoach = poloniex.Coach()
+    #    self.polo = poloniex.Poloniex(coach=myCoach)
+    #    self.polo2 = poloniex.Poloniex(coach=myCoach)
+    #    self.assertTrue(self.polo.coach is self.polo2.coach)
 
     def test_PoloniexErrors(self):
         self.polo = poloniex.Poloniex()
