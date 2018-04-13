47930be769d1aadfec55a9f2aa43824e53908a3e
Rafael Ribeiro <rafaelektron@gmail.com>
2018-04-13 02:05:49 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 setup(name='poloniex',
-      version='0.4.7',
+      version='0.4.8',
       description='Poloniex API wrapper for Python 2.7 and 3',
-      url='https://github.com/s4w3d0ff/python-poloniex',
+      url='https://github.com/rafaelribeiro/python-poloniex',
       author='s4w3d0ff',
       license='GPL v2',
       packages=['poloniex'],
